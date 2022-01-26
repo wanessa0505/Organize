6e13ee51acd62a4fa3068ead33904ce4475a1a6e
LuizFernando07 <silva.fonseca@escolar.ifrn.edu.br>
2022-01-26 19:12:37 -0300

    atualizacao luiz

--- resources/views/templates/add-anotacao.blade.php
+++ resources/views/templates/add-anotacao.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="/css/add-anotacao.css">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adicionar anotação</title>
+    <title>Adicionar Anotação</title>
 
 </head>
 
